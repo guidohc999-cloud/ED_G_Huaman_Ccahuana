e87b407328b597be117cfb7045e0fbf3aac5f9dc
user.name <guidohc999@gmail.com>
2025-08-30 18:02:45 -0500

    Evaluación diagnóstica completa

---
+++ Evaluacion Diagnostica/Ejerc-1/Ejerc-1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Ejercicio1
+{
+    static void Main()
+    {
+        Console.Write("Ingrese su año de nacimiento: ");
+        int anioNacimiento = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese el año actual: ");
+        int anioActual = int.Parse(Console.ReadLine());
+
+        int edad = anioActual - anioNacimiento;
+
+        Console.WriteLine($"Tu edad es: {edad}");
+
+        if (edad >= 18)
+            Console.WriteLine("Eres mayor de edad.");
+        else
+            Console.WriteLine("Eres menor de edad.");
+    }
+}
+
