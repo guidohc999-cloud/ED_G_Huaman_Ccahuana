49b5ac7e118f1c08879f97c326214ac62513ef5e
guidohc999-cloud <guidohc999@gmail.com>
2025-08-31 20:04:07 -0500

    Add files via upload

---
+++ Evaluacion Diagnostica/Ejerc- 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese nota de Examen 1: ");
+        double e1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese nota de Examen 2: ");
+        double e2  = double.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese nota de Examen 3: ");
+        double e3  = double.Parse(Console.ReadLine());
+
+        double promedio = (e1 * 0.3) + (e2 * 0.3) + (e3 * 0.4);
+
+        Console.WriteLine($"Promedio ponderado: {promedio:F2}");
+
+
+    }
+}
